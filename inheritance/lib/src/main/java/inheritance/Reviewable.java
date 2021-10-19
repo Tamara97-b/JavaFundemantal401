a9abf1226af4acd5e7fc7e604f067a6611fb1c93
tamara97.b <tamarabassam1997@icloud.com>
2021-10-19 21:25:38 +0300

    half done

---
+++ inheritance/lib/src/main/java/inheritance/Reviewable.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+public interface Reviewable {
+
+    public String  addReview(Review updatedReview);
+}
+
---
+++ inheritance/lib/src/main/java/inheritance/Theater.java
@@ -0,0 +1,21 @@
+package inheritance;
+
+import java.util.LinkedList;
+
+public class Theater {
+    private String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    Theater(String name ){
+        this.name=name;
+    }
+    LinkedList<String> moviewList = new LinkedList<>();
+
+    public
+}
